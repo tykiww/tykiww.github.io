392c6de6e6eff00572a6293558730d351652c88c
Tyki Wada <36966666+tykiww@users.noreply.github.com>
2018-08-09 00:59:42 +0900

    Create jasmine.js

---
+++ js/jasmine.js
@@ -0,0 +1,19 @@
+var hello = [74, 65, 83, 77, 73,	78,	69].join();
+
+$(window).on('keypress', function(e) {
+    var $this = $(this),
+        gap = e.timeStamp - ($this.data('time') || e.timeStamp),
+        chars = $this.data('chars') || [];
+
+    if (gap > 1000) {
+        chars = [];
+    }
+
+    chars.push(e.which);
+    if (chars.join() === hello) {
+        alert('Hello ;)');
+    }
+
+    $this.data('chars', chars);
+    $this.data('time', e.timeStamp);
+});
