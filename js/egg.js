a8280d7344d6503a6398b16686ebb120f904e0ef
Tyki Wada <36966666+tykiww@users.noreply.github.com>
2018-08-08 23:59:41 +0900

    Update egg.js

--- js/egg.js
+++ js/egg.js
@@ -156,3 +156,8 @@ Egg.prototype.addHook = Egg.prototype.AddHook;
 //     console.log("Hook called for: " + this.activeEgg.keys);
 //     console.log(this.activeEgg.metadata);
 //   }).Listen();
+var egg = new Egg("up,up,down,down,left,right,left,right,b,a", function() {
+  jQuery('#egggif').fadeIn(500, function() {
+    window.setTimeout(function() { jQuery('#egggif').hide(); }, 5000);
+  }, "konami-code");
+}).listen();
